c020ac3ad1f64bea2ed2a63640226c36c18f43ba
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-21 13:08:01 -0300

    Initial commit

---
+++ NoLookingTest-2026-09-21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//using Microsoft C# Documentation only
+//no looking at old projects or old code
+
+//Combine what i know so far
+//make small game where player can roll for damage + heal
+//have a YouWin outcome and a YouLoose outcome
+//have playerHealth and enemyHealth
+//give both player and enemy a HUD either separate or together
+//Combine it together with XP and LevelSystem 
+//to make it a play till you loose game
+
+//does not have time limit, just requires me to only use -
+//- it when it's been at least an hour since i've looked at code
+
+//just realized i have no idea how to make this repeat itself
+//but im sure i can figure it out lol
+
+namespace NoLookingTest_2026_09_21
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
